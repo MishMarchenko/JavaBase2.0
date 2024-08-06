e5724e9ba4545a1863fcb8053d8d2e722ab98c81
MishMarchenko <mishmar4enko@gmail.com>
2024-08-06 15:27:57 +0300

    arrays started

---
+++ src/Arrays_and_loops.java
@@ -0,0 +1,22 @@
+public class Arrays_and_loops {
+    public static void main(String[] args) {
+
+        int [] array = new int[10];
+
+        int array2 [] = new int[10];
+
+        int array3 [] = new int[] {1, 2, 3, 4, 5};
+
+        int [] array4 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        int arrayM [] [] = new int[3][3];
+
+        int arrayM2 [] [] = {
+                {1, 2, 3, 4},
+                {5, 6, 7},
+                {8, 9, 10, 11, 12, 13, 14},
+                            };
+
+
+    }
+}
